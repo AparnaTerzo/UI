654ec222773817d0808d20973ca28b2fe92bdb8c
AparnaTerzo <136312516+AparnaTerzo@users.noreply.github.com>
2023-06-20 14:38:58 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,43 @@
+console.log("Hello world");
+let name = "aparna";
+name="anu";
+console.log(name);
+const interest = 0.16;
+console.log(interest);
+console.log(typeof(name));
+
+console.log(typeof(interest));
+
+let person={
+    name:"abi",
+    age:30
+}
+console.log(person);
+person.name = "john";
+console.log(person.name);
+
+person['name']="dharun";
+console.log(person.name);
+
+//array
+let color=['red','blue','green']
+console.log(color[0]);
+
+//function
+
+function greet(name){
+    console.log(name);
+}
+greet('nuu');
+function square(number){
+    return number*number;
+}
+console.log(square(9));
+
+
+
+function myfunction(){
+    var a=5;
+ console.log(a);
+}
+console.log(a);
